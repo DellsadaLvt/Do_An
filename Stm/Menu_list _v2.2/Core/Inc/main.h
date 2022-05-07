7a2e70d2d28594440eee898d06d22a2b672a9cd7
DellsadaLvt <levantho0168@gmail.com>
2022-05-07 14:59:11 +0700

    upload Menulist v2.2

---
+++ Stm/Menu_list _v2.2/Core/Inc/main.h
@@ -0,0 +1,97 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Enable_step_Pin GPIO_PIN_4
+#define Enable_step_GPIO_Port GPIOA
+#define CLK_loadcell_Pin GPIO_PIN_5
+#define CLK_loadcell_GPIO_Port GPIOA
+#define Data_loadcell_Pin GPIO_PIN_6
+#define Data_loadcell_GPIO_Port GPIOA
+#define glcd_clk_Pin GPIO_PIN_0
+#define glcd_clk_GPIO_Port GPIOB
+#define glcd_data_Pin GPIO_PIN_1
+#define glcd_data_GPIO_Port GPIOB
+#define CB5_Pin GPIO_PIN_11
+#define CB5_GPIO_Port GPIOB
+#define CL1_Pin GPIO_PIN_12
+#define CL1_GPIO_Port GPIOB
+#define CL2_Pin GPIO_PIN_13
+#define CL2_GPIO_Port GPIOB
+#define CL3_Pin GPIO_PIN_14
+#define CL3_GPIO_Port GPIOB
+#define CB1_Pin GPIO_PIN_15
+#define CB1_GPIO_Port GPIOB
+#define CB2_Pin GPIO_PIN_8
+#define CB2_GPIO_Port GPIOA
+#define CB3_Pin GPIO_PIN_11
+#define CB3_GPIO_Port GPIOA
+#define CB4_Pin GPIO_PIN_12
+#define CB4_GPIO_Port GPIOA
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Stm/Menu_list _v2.2/MDK-ARM/User/Include/glcd.h
@@ -0,0 +1,84 @@
+#ifndef _USER_GLCD_H_
+
+#define _USER_GLCD_H_
+
+
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+#ifndef LINE_0_BASE_DDRAM
+	#define LINE_0_BASE_DDRAM							((uint8_t)0x80)  
+#endif
+
+#ifndef LINE_1_BASE_DDRAM
+	#define LINE_1_BASE_DDRAM							((uint8_t)0x90)  
+#endif
+
+#ifndef LINE_2_BASE_DDRAM
+	#define LINE_2_BASE_DDRAM							((uint8_t)0x88)  
+#endif
+
+#ifndef LINE_3_BASE_DDRAM
+	#define LINE_3_BASE_DDRAM							((uint8_t)0x98)  
+#endif
+
+
+
+#ifndef HORIZONTAL_ADDRESS_BASE
+	#define HORIZONTAL_ADDRESS_BASE			((uint8_t)0x80)  
+#endif
+
+#ifndef VERTICAL_ADDRESS_BASE
+	#define VERTICAL_ADDRESS_BASE				((uint8_t)0x80)  
+#endif
+
+#ifndef GDRAM_LINE
+	#define GDRAM_LINE(xx) 		((uint8_t)(VERTICAL_ADDRESS_BASE + 8u*(uint8_t)(xx)))  // xx from 0-> 3
+#endif
+
+#ifndef BEGIN_LINE
+	#define BEGIN_LINE 		HORIZONTAL_ADDRESS_BASE
+#endif
+
+/* Using glcd gpio mode transfer */
+#define GLCD_GPIO
+
+
+/* General functions */
+HAL_StatusTypeDef glcd_Init( void );
+HAL_StatusTypeDef glcd_Start(void);
+HAL_StatusTypeDef glcd_Stop(void);
+
+/* Basic instructions */
+HAL_StatusTypeDef glcd_entry_basic_mode( void );
+HAL_StatusTypeDef glcd_display_clear(void);
+HAL_StatusTypeDef glcd_basic_print_string(const uint8_t position, const uint8_t *const str, const uint8_t str_len);
+
+/* Graphic functions */
+HAL_StatusTypeDef glcd_entry_graphic_mode( void );
+HAL_StatusTypeDef glcd_clear_graphic(void);
+HAL_StatusTypeDef glcd_print_image(const unsigned char *const image);
+HAL_StatusTypeDef glcd_graphic_print_characters(const uint8_t vertical, const uint8_t horizontal, const uint8_t *str, unsigned char str_len);
+HAL_StatusTypeDef glcd_graphic_print_digit(const uint8_t vertical, const uint8_t horizontal, const uint8_t *str, unsigned char str_len);
+
+/* Test function */
+HAL_StatusTypeDef glcd_test(void);
+HAL_StatusTypeDef glcd_test_CGRAM(void);
+HAL_StatusTypeDef glcd_test_graphic_mode(void);
+
+#endif   /* _USER_GLCD_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
